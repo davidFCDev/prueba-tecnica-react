89503263fb51acf161d8bae8dad5cad4b39496cd
David Fernández <davidfernandezcomesana@gmail.com>
2023-06-27 23:46:52 +0200

    fetching listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from 'react'
 
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red$json=true`
+
 const App = () => {
   const [fact, setFact] = useState('')
 
   useEffect(() => {
-    fetch('https://catfact.ninja/fact'
+    fetch(CAT_ENDPOINT_RANDOM_FACT
     ).then(response => response.json()
-    ).then(data => setFact(data.fact))
+    ).then(data => {
+      const { fact } = data
+      setFact(fact)
+
+      const firstWord = fact.split(' ', 3)
+      console.log(firstWord)
+    })
   }, [])
 
   return (
     <main>
       <h1>App de gatitos</h1>
-      <p>{fact}</p>
+      {fact && <p>{fact}</p>}
     </main>
   )
 }
