4fd47f2255c80fa217dfa39ddf7bb02a9c6a3538
David Fernández <davidfernandezcomesana@gmail.com>
2023-06-29 12:28:30 +0200

    añadido test

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { useCatImage } from './hooks/useCatImage'
+import { useCatFact } from './hooks/useCatFact'
 import './App.css'
-import { getRandomFact } from './services/facts'
-
-const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
-
-function useCatImage ({ fact }) {
-  const [imageUrl, setImageUrl] = useState('')
-
-  useEffect(() => {
-    if (!fact) return
-    const threeFirstWords = fact.split(' ', 3).join(' ')
-    fetch(
-      `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`
-    )
-      .then((res) => res.json())
-      .then((response) => {
-        const { url } = response
-        setImageUrl(url)
-      })
-  }, [fact])
-
-  return { imageUrl }
-} // { imageUrl: 'https:...'}
 
 const App = () => {
-  const [fact, setFact] = useState('')
+  const { fact, refreshFact } = useCatFact()
   const { imageUrl } = useCatImage({ fact })
 
-  useEffect(() => {
-    getRandomFact().then(setFact)
-  }, [])
-
   const handleClick = async () => {
-    const newFact = await getRandomFact(setFact)
-    setFact(newFact)
+    refreshFact()
   }
 
   return (
@@ -44,7 +19,7 @@ const App = () => {
         {fact && <p>{fact}</p>}
         {imageUrl && (
           <img
-            src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`}
+            src={imageUrl}
             alt={`Image extracted using the first three words for ${fact}`}
           />
         )}
